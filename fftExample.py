09840d6b12932f57c65376e9d1f4f118a8683574
jtenbarg <jason.tenbarge@gmail.com>
2022-06-15 16:04:21 -0400

    Improve polar FFT functionality.

--- fftExample.py
+++ fftExample.py
@@ -2,8 +2,7 @@ import numpy as np
 import scipy as sp
 from utils import gkData
 from utils import gkPlot
-from utils import initpolar
-from utils import polar_isotropic
+from utils import polarFFT
 import matplotlib.pyplot as plt
 import postgkyl as pg
 params = {} #Initialize dictionary to store plotting and other parameters
@@ -24,12 +23,13 @@ plt.rcParams["text.usetex"] = True
 
 
 #Tested to handle g0 and g2: VM, 5M, 10M
-#Requires a _params.txt file in your data directory or the form gkeyllOutputBasename_params.txt! See example_params.txt for formatting
-paramsFile = '/Users/jtenbarg/Desktop/runs/gemEddyv43/Data/gem_params.txt' 
+#Requires a _params.txt file in your data directory of the form gkeyllOutputBasename_params.txt! See example_params.txt for formatting
+paramsFile = '/Users/jtenbarg/Desktop/runs/gemEddyv43/Data/gem_params.txt'
+paramsFile = '/Users/jtenbarg/Desktop/Runs/ot3D_2D/ot3D_params.txt'
 
-fileNum = 19
+fileNum = 100
 suffix = '.bp'
-bgDir = 2
+bgDir = 2 #Guide field direction: 0, 1, or 2 = x, y, or z. If 2D data, this direciton must not be in the 2D plane.
 tmp = gkData.gkData(paramsFile,fileNum,suffix,'bx',params) #Initialize constants for normalization
 
 #below limits [z0, z1, z2,...] normalized to params["axesNorm"]
@@ -43,7 +43,7 @@ refSpeciesAxes = 'ion'
 refSpeciesTime = 'ion'
 speciesIndexAxes = tmp.speciesFileIndex.index(refSpeciesAxes)
 speciesIndexTime = tmp.speciesFileIndex.index(refSpeciesTime)
-params["axesNorm"] = [tmp.d[speciesIndexAxes], tmp.d[speciesIndexAxes], tmp.vt[speciesIndexAxes], tmp.vt[speciesIndexAxes], tmp.vt[speciesIndexAxes]]
+params["axesNorm"] = [tmp.d[speciesIndexAxes], tmp.d[speciesIndexAxes], tmp.d[speciesIndexAxes]]
 params["timeNorm"] = tmp.omegaC[speciesIndexTime]
 
 
@@ -55,22 +55,16 @@ dataShape = np.shape(bx.data)
 dims = len(dataShape)
 volumeFFT = np.prod(dataShape)
 
-k = [np.fft.fftfreq(dataShape[d], (bx.coords[d][1] - bx.coords[d][0])/(2*np.pi*bx.params["axesNorm"][d])) for d in range(dims)]
+k = [np.fft.fftfreq(dataShape[d], (bx.coords[d][1] - bx.coords[d][0])/(2*np.pi*bx.params["axesNorm"][d])) for d in range(dims)] #Creat wavenumber array
 
-#Setup bins and wavenumbers for perpendicular polar spectra
-perpind = np.squeeze(np.where(np.arange(dims) != bgDir))
-nkp = np.array([dataShape[perpind[d]] for d in range(len(perpind))])
-kperp = [k[perpind[d]] for d in range(len(perpind))]
-nkpolar = int(np.floor(np.sqrt( np.sum(((nkp)/2)**2) )))
-akp, nbin, polar_index, akplim = initpolar.initpolar(nkp[0], nkp[1], 0, kperp[0], kperp[1], [0], nkpolar)
-ebinCorr = np.pi*akp/(akp[0]*nbin)
+akp, nbin, polar_index, akplim, ebinCorr = polarFFT.initPolar(k, bgDir) #Initialize polar wavenumber bins
 
 #Read and take FFT of data
 bx = gkData.gkData(paramsFile,fileNum,suffix,'bx',params).compactRead()
 bxFFT = np.fft.fftn(bx.data) * np.sqrt(1/volumeFFT)
 
-#Compute reduced polar spectra
-EbxPol = polar_isotropic.polar_isotropic(nkpolar, dataShape[0], dataShape[1], 0, polar_index, bxFFT, ebinCorr)
+#Compute reduced polar spectrum
+EbxPol = polarFFT.polarFFTBin(k, bgDir, polar_index, bxFFT)
 
 #Plot spectrum
 plt.figure(figsize=(12,8))
---
+++ utils/polarFFT.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+
+def initPolar(k, bgDir):
+
+    dims = np.shape(k)[0]
+    
+    if dims == 1: 
+        akp = [] 
+        nbin = 0
+        polar_index = []
+        akplim = []
+    else:
+        #Setup bins and wavenumbers for perpendicular polar spectra
+        perpind = np.squeeze(np.where(np.arange(dims) != bgDir))
+        nkp = np.array([len(k[d]) for d in range(len(perpind))])
+        kperp = [k[perpind[d]] for d in range(len(perpind))]
+        nkpolar = int(np.floor(np.sqrt( np.sum(((nkp)/2)**2) )))
+        nkx = nkp[0]
+        nky = nkp[1]
+        
+        nbin = np.zeros(nkpolar) #Number of kx,ky in each polar bins
+        polar_index = np.zeros((nkx, nky), dtype=int) #Polar index to simplify binning 
+        if nkx == 1 & nky==1:
+            dkp = 0
+        elif nkx == 1:
+            dkp = kperp[1]
+        elif nky == 1:
+            dkp = kperp[0]
+        else:
+            dkp = max(kperp[0][1], kperp[1][1])
+        akp = (np.linspace(1, nkpolar, nkpolar))*dkp #Kperp grid
+        akplim = dkp/2 + (np.linspace(0,nkpolar, nkpolar+1))*dkp #Bin limits
+        #Re-written to avoid loops. Necessary for large grids.
+        [kxg, kyg] = np.meshgrid(kperp[1],kperp[0]) #Deal with meshgrid weirdness (so do not have to transpose)
+        kp = np.sqrt(kxg**2 + kyg**2)
+        pn  = np.where(kp >= akplim[nkpolar])
+        polar_index[pn[0], pn[1]] = nkpolar-1  
+        nbin[nkpolar-1] = nbin[nkpolar-1] + len(pn[0])
+        for ik in range(0, nkpolar):
+            pn = np.where((kp < akplim[ik+1]) & (kp >= akplim[ik]))
+            polar_index[pn[0], pn[1]] = ik
+            nbin[ik] = nbin[ik] + len(pn[0])
+
+        nbinDiv = nbin.copy()
+        nbinDiv[np.where(nbinDiv==0)]=1 #Deal with divide by zero in ebinCorr
+        ebinCorr = np.pi*akp/(akp[0]*nbinDiv)
+        
+    return akp, nbin, polar_index, akplim, ebinCorr
+
+
+def polarFFTBin(k, bgDir, polar_index, fft_matrix):
+    
+    dims = np.shape(k)[0]
+    perpind = np.squeeze(np.where(np.arange(dims) != bgDir))
+    nkp = np.array([len(k[d]) for d in range(len(perpind))])
+    nkpolar = int(np.floor(np.sqrt( np.sum(((nkp)/2)**2) )))
+
+    if dims == 1:
+        fft_isok = [] 
+    elif dims == 2:
+        fft_isok = np.zeros(nkpolar)
+    
+        for i in range(0, nkp[0]):
+            for j in range(0, nkp[1]):
+                ikperp = polar_index[i,j]
+                fft_isok[ikperp] = fft_isok[ikperp] + np.abs(fft_matrix[i,j])**2
+    else:
+        nkb = bgDir
+        fft_isok = np.zeros(nkpolar, nkb)
+
+        newFFTMatrix = np.transpose(fft_matrix, (perpind[0], perpind[1], bgDir)) #Re-order matrix into [perp1, perp2, bgDir]
+        
+        for i in range(0, nkp[0]):
+            for j in range(0, nkp[1]):
+                for k in range(0, nkb):
+                    ikperp = polar_index[i,j]
+                    fft_isok[ikperp,k] = fft_isok[ikperp,k] + np.abs(newFFTMatrix[i,j,k])**2
+
+    return fft_isok
